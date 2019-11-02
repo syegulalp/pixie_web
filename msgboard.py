8557d4f85ae6f53d31f2743a1d4272b0bcd6258c
Serdar Yegulalp <serdar@genjipress.com>
2019-11-02 00:31:36 -0400

    update msgboard demo

--- msgboard.py
+++ msgboard.py
@@ -8,8 +8,12 @@ main_template = template("post.html", "demo")
 
 # because `shelve` is not threadsafe, we must run this demo single-threaded
 
+
 @route("/", ProcessType.main)
 def main(env):
+
+    msg = ""
+
     with shelve.open("db", flag="c", protocol=5) as db:
 
         if not "posts" in db:
@@ -20,13 +24,16 @@ def main(env):
 
         if env.headers["_VERB"] == "POST":
             value = env.form["name"]
-            data.append(value)
-            if len(data) > 5:
-                data.pop(0)
-            db["posts"] = data
+            if value != "":
+                data.append(value)
+                if len(data) > 5:
+                    data.pop(0)
+                db["posts"] = data
+            else:
+                msg = "<p>You submitted a blank post. Be more creative.</p>"
 
         text = main_template.format(
-            "<ul>" + "".join([f"<li>{_}</li>" for _ in db["posts"]]) + "</ul>"
+            "<ul>" + "".join([f"<li>{_}</li>" for _ in db["posts"]]) + "</ul>", msg
         )
 
     return response(text)
